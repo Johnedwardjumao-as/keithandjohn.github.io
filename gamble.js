95a831d6e2845c7ef61028e59e8e83a410869b64
Johnedwardjumao-as <149262615+Johnedwardjumao-as@users.noreply.github.com>
2023-12-02 18:30:51 +0800

    Add files via upload

---
+++ gamble.js
@@ -0,0 +1,38 @@
+var balance = prompt("pila imo kwarta?");
+
+        function getRandomColor() {
+            var colors = ['yellow', 'white', 'violet', 'blue', 'red', 'green'];
+            var randomIndex = Math.floor(Math.random() * colors.length);
+            return colors[randomIndex];
+        }
+
+        function playGame() {
+            var chosenColor;
+
+            while (true) {
+                chosenColor = prompt("Choose a color (yellow, white, violet, blue, red, green):");
+
+                if (chosenColor && chosenColor.toLowerCase() === 'yellow' || chosenColor.toLowerCase() === 'white' || chosenColor.toLowerCase() === 'violet' || chosenColor.toLowerCase() === 'blue' || chosenColor.toLowerCase() === 'red' || chosenColor.toLowerCase() === 'green') {
+                    break;
+                } else {
+                    alert("wala sa pili-anan bobo");
+                }
+            }
+
+            var gameColor = getRandomColor();
+            document.getElementById('gameColor').style.backgroundColor = gameColor;
+
+            if (chosenColor.toLowerCase() === gameColor) {
+                balance *= 2;
+                alert("hayahaya sa boang oi.  balance: " + balance + " pesos.");
+            } else {
+                balance -= 5;
+                alert("Wrong! Try again. balance: " + balance + " pesos.");
+            }
+
+            updateBalance();
+        }
+
+        function updateBalance() {
+            document.getElementById('balance').textContent = "Balance: " + balance + " pesos";
+        }
